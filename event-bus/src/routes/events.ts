8e84d30c30ebc7999c834783f270fcf9b1424667
marciomarquessouza <marciomultimedia@gmail.com>
2021-02-13 21:17:15 -0300

    feat: handle missing events

--- event-bus/src/routes/events.ts
+++ event-bus/src/routes/events.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import axios from "axios";
 
+const events = [];
+
 export const eventsRoutes = (router: Router) => {
   router.post("/events", (req, res) => {
     const event = req.body;
+
+    events.push(event);
+
     axios.post("http://localhost:4000/events", event);
     axios.post("http://localhost:4001/events", event);
     axios.post("http://localhost:4002/events", event);
     axios.post("http://localhost:4003/events", event);
+
     res.send({ status: "ok" });
+  });
+
+  router.get("/events", (req, res) => {
+    res.send(events);
   });
 };
